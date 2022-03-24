e2261d83d5153a603f8ba74ea3869d739aaa9569
saranbabu33 <101859378+saranbabu33@users.noreply.github.com>
2022-03-24 15:24:28 +0530

    Create bios.groovy

---
+++ vars/bios.groovy
@@ -0,0 +1,38 @@
+node ('Win10') {
+   repo_config = [
+      "Lagos" : [
+
+                  "arti" : "foxconn,inventec,wistron,insyde",
+                  "generation" : "16G",
+                  "email" : aaaa@dell.com,
+                  "branch" : "main",
+                  "script" : "",
+                ],
+
+      "EagleMnt" : [
+                  "arti" : "foxconn",
+                  "generation" : "16G",
+                  "email" : bbbb@dell.com
+                  "branch" : "main",
+                  "script" : "",
+                               "build_dir" : "Build_Genoa",
+                ],
+
+      "Brazos" : [
+                  "arti" : "foxconn",
+                  "generation" : "16G",
+                  "email" : bbbb@dell.com
+                  "branch" : "main",
+                  "script" : "",
+                               "build_dir" : "Build_Genoa",
+                ],
+
+      "MonteCarlo" : [
+                  "arti" : "foxconn,inventec,wistron,compal",
+                  "generation" : "15G",
+                  "email" : bbbb@dell.com
+                  "branch" : "stg/15g_main",
+                  "script" : "",
+                ],
+    ]
+  }
