c0c2223a9914f448a2279df1b83a000116af8a1e
saranbabu33 <101859378+saranbabu33@users.noreply.github.com>
2022-03-24 16:14:19 +0530

    Update bios.groovy

--- vars/bios.groovy
+++ vars/bios.groovy
@@ -2,9 +2,8 @@ node ('Win10') {
 
   echo "Initial multi-thread pipeline..."
 
-}
 
-teamsUrl = https://dell.webhook.office.com/webhookb2/68f3dac6-cf38-42a5-a4bf-69adfc1869dc@945c199a-83a2-4e80-9f8c-5a91be5752dd/IncomingWebhook/1922f62fd23341a59a5c85ee259a4e3f/7aadc77b-2395-46d5-93fe-a4e5ec6a2b99
+teamsUrl = "https://dell.webhook.office.com/webhookb2/68f3dac6-cf38-42a5-a4bf-69adfc1869dc@945c199a-83a2-4e80-9f8c-5a91be5752dd/IncomingWebhook/1922f62fd23341a59a5c85ee259a4e3f/7aadc77b-2395-46d5-93fe-a4e5ec6a2b99"
 
    repo_config = [
       "Lagos" : [
