89e0982fc15b35122b9177a845e05fed0c51a049
saranbabu33 <101859378+saranbabu33@users.noreply.github.com>
2022-04-03 12:44:12 +0530

    Update bios.groovy

--- vars/bios.groovy
+++ vars/bios.groovy
@@ -1,18 +1,2 @@
 def call(){
-    node ('Win10') {
-        
-        stage('Preparation') { // for display purposes
-        echo "Preparation"
-        checkout scm
-        test.setEnableFlag(true)
-        test.execute()
-        helper.performAction()
-        }
-        stage('Build') {
-        echo "Build"
-        }
-        stage('Results') {
-            echo "Results"
-        }
-    }
 }
