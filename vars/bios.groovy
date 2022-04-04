3746e0a7b2cb2bb5e98dfa0cf57fb6ea6fb4ca6a
saranbabu33 <101859378+saranbabu33@users.noreply.github.com>
2022-04-04 10:33:51 +0530

    Update bios.groovy

--- vars/bios.groovy
+++ vars/bios.groovy
@@ -1,2 +1,54 @@
-def call(){
-}
+def call(Map config=[:]){
+    node {
+        settings.config(config)
+    }
+
+    pipeline {
+        agent any
+
+        options {
+            //timestamps()
+            skipDefaultCheckout()
+            parallelsAlwaysFailFast()
+            timeout(time: 60, unit: 'MINUTES')
+            buildDiscarder(logRotator(numToKeepStr: '30'))
+        }
+
+        parameters {
+            gitParameter (
+                branch: '', 
+                branchFilter: 'origin/(.*)', 
+                defaultValue: 'master', 
+                listSize: '10', )
+               
+
+            choice(
+                name: 'Lagos',
+                description: 'Please select Environment',
+                choices: 'dev\nqa\nuat\npre\nprd')
+
+            choice(
+                name: 'EagleMnt',
+                description: 'Please select action',
+                choices: 'deploy\nrollback')
+
+            choice(
+                name: 'Brazos',
+                description: 'Please select action',
+                choices: 'Container\nLegacy\nMixed')
+        }
+
+        stages {
+            stage ('Initial Pipeline') {
+                steps {
+                    script {
+                        log.i 'Initiating Pipeline'
+                        controller.entry()
+                    }
+                }
+            }
+        }
+
+        
+            }
+        }
