f67a8ddff5cfa9a4ee62589a3ff043f4f31a83ce
saranbabu33 <101859378+saranbabu33@users.noreply.github.com>
2022-04-09 12:32:12 +0530

    Update bios.groovy

--- vars/bios.groovy
+++ vars/bios.groovy
@@ -1,66 +1,12 @@
-import groovy.transform.Field
-import groovy.json.JsonSlurper
-import groovy.json.JsonBuilder
-import jenkins.model.*
-    
-    class Config implements Serializable {
-    static Map data     = [
-        locale          : 'bios'
-    ]
+class my_vars {
+    static Map varMap = [:]
+    static def loadVars (Map config) {
+        varMap.var1 = "val1"
+        varMap.var2 = "val2"
+        varMap.var1 = "val3"
+        varMap.var2 = "val4"
+        // Many more variables ...
 
-    static Map settings = [:]
-}
-
-def call(Map config=[:]){
-    node {
-        settings.config(config)
+        return varMap
     }
-
-    pipeline {
-        agent any
-
-        options {
-            //timestamps()
-            skipDefaultCheckout()
-            parallelsAlwaysFailFast()
-            timeout(time: 60, unit: 'MINUTES')
-            buildDiscarder(logRotator(numToKeepStr: '30'))
-        }
-
-         /*parameters {
-            gitParameter (
-                branch: '', 
-                branchFilter: 'origin/(.*)', 
-                defaultValue: 'master', 
-                listSize: '10', ) */
-               
-
-            choice(
-                name: 'Lagos',
-                description: 'Please select Environment',
-                choices: 'dev\nqa\nuat\npre\nprd')
-
-            choice(
-                name: 'EagleMnt',
-                description: 'Please select action',
-                choices: 'deploy\nrollback')
-
-            choice(
-                name: 'Brazos',
-                description: 'Please select action',
-                choices: 'Container\nLegacy\nMixed')
-        }
-
-        stages {
-            stage ('Initial Pipeline') {
-                steps {
-                    script {
-                        log.i 'Initiating Pipeline'
-                        controller.entry()
-                    }
-                }
-            }
-        }
-
-        
-            }
+}
