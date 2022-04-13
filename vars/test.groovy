e77f6dc29058fd611fa5c495bdf8a4e076d53df5
saranbabu33 <101859378+saranbabu33@users.noreply.github.com>
2022-04-13 12:54:08 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,5 +1,14 @@
-def function () {
+def buildApp () {
     echo 'building the application'
+}
+
+def testApp () {
+    echo 'building the application'
+}
+
+def deployApp () {
+    echo 'building the application'
+    echo "deploying repo ${params.Repo}"
 }
 
 return this
